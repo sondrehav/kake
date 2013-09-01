719ddef9358f582aced38d4f4d40f5b98c267ec1
sondrehav <sondre.havellen@gmail.com>
2013-09-01 19:22:25 +0200

    Enemy class
    - Enemy class
    - Basic movement

--- Project02/src/module02.py
+++
@@ -1,8 +0,0 @@
-import pygame, sys
-from pygame.locals import *
-
-pygame.init()
-fpsClock = pygame.time.Clock()
-
-windowSurfaceObj = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Awesome')
