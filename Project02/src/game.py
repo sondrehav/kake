89c2c8c5dc4b50558ed174a2aafbadc3276a5d7a
martinhath <martinhath@gmail.com>
2013-09-01 18:15:59 +0200

    Lagde en player klasse fra kjørefilen. Kollisjon oppe og nede med kantene på skjermen er ikke implementert.

--- Project02/src/game.py
+++ Project02/src/game.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from pygame.locals import *
 
+from Player import *
+
 width, height = 640, 480    
 
 pygame.init()
@@ -9,112 +11,49 @@ fpsClock = pygame.time.Clock()
 windowSurfaceObj = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Pygame Cheat Sheet')
 
-catSurfaceObj = pygame.image.load('cat.png')
 redColor = pygame.Color(255, 0, 0)
 greenColor = pygame.Color(0, 255, 0)
 blueColor = pygame.Color(0, 0, 255)
 whiteColor = pygame.Color(255, 255, 255)
 blackColor = pygame.Color(0, 0, 0)
 mousex, mousey = 0, 0
-playerXVel, playerYVel = 0, 0
-playerXPos, playerYPos = 50, 50
 
-catMaxSpeed = 5
-catAcc = 0.25
-
-catMoveLeft, catMoveRight, catMoveUp, catMoveDown = False, False, False, False
+player = player()
+player.xpos = 100
+player.ypos = 100
 
 fontObj = pygame.font.Font('freesansbold.ttf', 32)
-msg = 'Katt'
 
 while True:
     windowSurfaceObj.fill(whiteColor)
+    events = pygame.event.get()
 
-    windowSurfaceObj.blit(catSurfaceObj, (playerXPos, playerYPos))
-    
-
-    msgSurfaceObj = fontObj.render(msg, True, blueColor)
-    msgRectobj = msgSurfaceObj.get_rect()
-    msgRectobj.topleft = (width/2 - (msgRectobj.width/2), 10)
-    windowSurfaceObj.blit(msgSurfaceObj, msgRectobj)
-
-    for event in pygame.event.get():
+    for event in events:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == MOUSEMOTION:
             mousex, mousey = event.pos
-        elif event.type == MOUSEBUTTONUP:
-            mousex, mousey = event.pos
-            
-        elif event.type == KEYDOWN:
-            if event.key == K_a:
-                catMoveLeft = True
-            if event.key == K_d:
-                catMoveRight = True
-            if event.key == K_s:
-                catMoveDown = True
-            if event.key == K_w:
-                catMoveUp = True
-                
-        elif event.type == KEYUP:
-            if event.key == K_a:
-                catMoveLeft = False
-            if event.key == K_d:
-                catMoveRight = False
-            if event.key == K_s:
-                catMoveDown = False
-            if event.key == K_w:
-                catMoveUp = False
-                
+        if event.type == KEYUP:
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
     
-    if catMoveLeft:
-        if playerXVel > -catMaxSpeed:
-            playerXVel -= catAcc
-    if catMoveRight:
-        if playerXVel < catMaxSpeed:
-            playerXVel += catAcc
-    if catMoveUp:
-        if playerYVel > -catMaxSpeed:
-            playerYVel -= catAcc
-    if catMoveDown:
-        if playerYVel < catMaxSpeed:
-            playerYVel += catAcc
-    
-    if catMoveLeft == False and catMoveRight == False:
-        if playerXVel > 0:
-            playerXVel -= 0.5
-        elif playerXVel < 0:
-            playerXVel += 0.5
-            
-    if catMoveUp == False and catMoveDown == False:
-        if playerYVel > 0:
-            playerYVel -= 0.5
-        elif playerYVel < 0:
-            playerYVel += 0.5
-    
-    playerXPos += playerXVel
-    playerYPos += playerYVel
-    
-    if playerXPos < 0:
-        playerXPos = 0
-        playerXVel = 0
+
+    player.update(events)
+
+    if player.xpos < 0:
+        player.xpos = 0
+        player.xvel = 0
+        player.moveLeft = False
+    elif player.xpos > width-player.width:
+        player.xpos = width-player.width
+        player.xvel =0
+        player.moveRight = False
 
 
-    if playerXPos > width - 50:
-        playerXPos = width - 50
-        playerXVel = 0
-    
-    if playerYPos < 0:
-        playerYPos = 0
-        playerYVel = 0
 
-    if playerYPos > height - 50:
-        playerYPos = height - 50
-        playerYVel = 0
 
+    player.render(windowSurfaceObj)
 
     pygame.display.update()
     fpsClock.tick(60) # pause to run the loop at 30 frames per second
